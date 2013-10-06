b04e8b18b1e5664332fb05d8b3e02a4c48e4d599
WSO2 <shan@Shans-MacBook-Air.local>
2013-10-06 20:42:07 +0530

    Initial Commit

---
+++ MDM AgentTests/MDM_AgentTests.h
@@ -0,0 +1,13 @@
+//
+//  MDM_AgentTests.h
+//  MDM AgentTests
+//
+//  Created by WSO2 on 10/6/13.
+//  Copyright (c) 2013 WSO2. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MDM_AgentTests : SenTestCase
+
+@end
---
+++ MDM AgentTests/MDM_AgentTests.m
@@ -0,0 +1,32 @@
+//
+//  MDM_AgentTests.m
+//  MDM AgentTests
+//
+//  Created by WSO2 on 10/6/13.
+//  Copyright (c) 2013 WSO2. All rights reserved.
+//
+
+#import "MDM_AgentTests.h"
+
+@implementation MDM_AgentTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MDM AgentTests");
+}
+
+@end
